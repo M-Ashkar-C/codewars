ea5c3674af71d2a924085259e5693f81d3189f1c
Ashkar <ashkarc13@gmail.com>
2024-08-30 10:08:15 +0530

    add another question

--- codewars.js
+++ codewars.js
@@ -1,5 +1,23 @@
-// Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+// You get an array of numbers, return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+let arr=[];
+sum =0;
+function positiveSum(arr) {
+    a1=arr.filter(value=>{
+      return value>0
+    })
+     s=a1.reduce((sum,value)=> sum+value,0);
+     return console.log(s)
+  }
+  positiveSum(arr)
+ 
+ 
 
+
+
+
+// Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
 //   3 -->    15  (  3 * 5¹)
 //  10 -->   250  ( 10 * 5²)
 // 200 --> 25000  (200 * 5³)
@@ -44,21 +62,20 @@
 
 
 // In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
-
 // Examples
 // makeNegative(1);    // return -1
 // makeNegative(-5);   // return -5
 // makeNegative(0);    // return 0
 // makeNegative(0.12); // return -0.12
 
-function makeNegative(num) {
-    // Code?
-    if(num>0){
-        let n=-num
-        return n
-    }else{
-        return num
-    }
-  }
-  console.log(makeNegative(-42));
-  
+// function makeNegative(num) {
+//     // Code?
+//     if(num>0){
+//         let n=-num
+//         return n
+//     }else{
+//         return num
+//     }
+//   }
+//   console.log(makeNegative(-42));
+   
